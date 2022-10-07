2a85bf718591a27301d80af8a72a3141cea72dbb
dandylion99 <446956866@qq.com>
2022-10-07 00:13:43 -0400

    Create map with data points
    explore two methods: streamlit and altair. Neither is perfect, and please see comments for details

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,31 +1,47 @@
+from turtle import back
 import streamlit as st
 import pandas as pd
 import altair as alt
+from vega_datasets import data
 
-st.title("Let's analyze some Penguin Data 🐧📊.")
+st.title("Social Capital Data Interaction.")
 
 @st.cache  # add caching so we load the data only once
 def load_data():
-    # Load the penguin data from https://github.com/allisonhorst/palmerpenguins.
-    penguins_url = "https://raw.githubusercontent.com/allisonhorst/palmerpenguins/v0.1.0/inst/extdata/penguins.csv"
-    return pd.read_csv(penguins_url)
+    return pd.read_csv("data/social_capital_zip_coords.csv")
 
 df = load_data()
 
 st.write("Let's look at raw data in the Pandas Data Frame.")
 
 st.write(df)
+st.write(len(df.index))
 
-st.write("Hmm 🤔, is there some correlation between body mass and flipper length? Let's make a scatterplot with [Altair](https://altair-viz.github.io/) to find.")
+# streamlit way to create map chart
+# allows zoom in/out, but seems not supporting tooltip on data points
+geo_data = df[['lat','lng']] # change column name, st.map() only recognize column names of "lat" and "lon"
+geo_data = geo_data.rename(columns={'lng':'lon'})
 
-chart = alt.Chart(df).mark_point().encode(
-    x=alt.X("body_mass_g", scale=alt.Scale(zero=False)),
-    y=alt.Y("flipper_length_mm", scale=alt.Scale(zero=False)),
-    color=alt.Y("species")
-).properties(
-    width=600, height=400
+st.map(geo_data).tooltip().interactive()
+
+# altair way to create map chart
+# altair supports tooltip on datapoints, but currently doesn't support to zoom in/out a map chart
+# reference: https://altair-viz.github.io/altair-tutorial/notebooks/09-Geographic-plots.html
+states = alt.topo_feature(data.us_10m.url, feature='states')
+
+background = alt.Chart(states).mark_geoshape(
+    fill='lightgray',
+    stroke='white'
+).project('albersUsa').properties(
+    width=500,
+    height=300
+)
+
+points = alt.Chart(df).mark_circle().encode(
+    longitude = 'lng',
+    latitude = 'lat',
+    size = alt.value(10),
+    tooltip = ['zip','lat','lng','ec_zip']
 ).interactive()
 
-st.write(chart)
-
-st.markdown("This project was created by Student1 and Student2 for the [Interactive Data Science](https://dig.cmu.edu/ids2022) course at [Carnegie Mellon University](https://www.cmu.edu).")
+st.write(background+points)
