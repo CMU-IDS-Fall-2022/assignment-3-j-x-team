2f7865c7915109257ae515b4a6b2f19346b90962
dandylion99 <446956866@qq.com>
2022-10-12 18:07:30 -0400

    Add text table and bar chart for the selected point

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,10 +9,6 @@ import numpy as np
 from dataclasses import fields
 from operator import index
 
-
-
-st.title("Cleaning & Praparing Data")
-
 ## **Data Cleaning**
 ## Step 1: The LAT and LNG data was added to the dataframe to enable each zip code to be plotted on a map.
 
@@ -30,41 +26,35 @@ st.title("Cleaning & Praparing Data")
 # sc_df.to_csv("social_capital_zip_coords.csv")
 
 # Load the resulting dataframe.
+@st.cache  # add caching so we load the data only once
 def load_data():
     return pd.read_csv("data/social_capital_zip_coords.csv")
 
 df = load_data()
 
 # Drop unnecessary columns 'County', 'Unnamed: 0', and 'index'.
-
 df = df.drop('county', axis=1)
 df = df.drop('Unnamed: 0', axis=1)
 df = df.drop('index', axis=1)
 
 # Make the zip code column a string to enable a later merge on the zip code column.
 df['zip'] = df['zip'].astype(str)
-
-df = df.set_index('zip')
-
 df = df.dropna()
 
 ## Step 2: Import a dataframe that contains the state, city, and county of each zip code.
-
 ssl._create_default_https_context = ssl._create_unverified_context
 url = urllib.request.urlopen('https://raw.githubusercontent.com/scpike/us-state-county-zip/master/geo-data.csv')
 city_state_df = pd.read_csv(url)
 
 city_state_df.rename(columns={'zipcode': 'zip'}, inplace=True)
 
-# Set the index to the zip code column for a later merge with the social capital dataframe.
-city_state_df.set_index('zip', inplace=True)
 
-# Step 3: Merge the two dataframes.  City, country, State, state_abbr, and state_fips are added as columns in the df.
-
+## Step 3: Merge the two dataframes.  City, country, State, state_abbr, and state_fips are added as columns in the df.
 df = pd.merge(df, city_state_df, on='zip')
+df['Location'] = df['state_abbr']+", "+df['county']+", "+df['city']
 
-# Step 4: Add a column for each percentile of each social capital dimension.
 
+## Step 4: Add a column for each percentile of each social capital dimension.
 ec_zip_percentile_list = []
 
 for i in df['ec_zip']:
@@ -72,7 +62,7 @@ for i in df['ec_zip']:
     percentile_val = round(percentile, 2)
     ec_zip_percentile_list.append(percentile_val)
 
-df['ec_zip_percentile'] = ec_zip_percentile_list
+df['Economic Connectedness Percentile'] = ec_zip_percentile_list
 
 clustering_zip_list = []
 
@@ -81,56 +71,60 @@ for i in df['clustering_zip']:
     percentile_val = round(percentile, 2)
     clustering_zip_list.append(percentile_val)
 
-df['clustering_zip_percentile'] = clustering_zip_list
+df['Clustering Percentile'] = clustering_zip_list
 
-civic_organizations_list = []
+volunteering_zip_list = []
 
-for i in df['civic_organizations_zip']:
-    percentile = stats.percentileofscore(df['civic_organizations_zip'], i, kind='strict')
+for i in df['volunteering_rate_zip']:
+    percentile = stats.percentileofscore(df['volunteering_rate_zip'], i, kind='strict')
     percentile_val = round(percentile, 2)
-    civic_organizations_list.append(percentile_val)
+    volunteering_zip_list.append(percentile_val)
 
-df['civic_organizations_zip_percentile'] = civic_organizations_list
+df['Volunteering Rate Percentile'] = volunteering_zip_list
 
-# Step 5: 
-
+## Step 5: 
 # Make 3 new columns that describe the meaning of each social capital metric. 
 # The value of each column is the same for each row.
 # The meaning of each metric will be displayed to the user on hover.
-
-df['ec_zip_meaning'] = 'Baseline definition of economic connectedness: two times the share of high-SES friends among low-SES individuals, averaged over all low-SES individuals in the ZIP code.'
-
-df['clustering_zip_meaning'] = 'The average fraction of an individual\s friend pairs who are also friends with each other.'
-
-df['civic_organizations_zip_meaning'] = 'The number of Facebook Pages predicted to be “Public Good” pages based on page title, category, and other page characteristics, per 1,000 users in the ZIP code.'
+df['zip_meaning'] = 'The zipcode you selected from the mao above'
+df['location_meaning'] = "Location of the zipcode, formatted in State, County, City"
+df['num_below_p50_meaning'] = "Number of children with below-national-median parental household income."
+df['population_2018_meaning'] = 'Population in 2018'
+df['ec_zip_meaning'] = "Two times the share of high-SES friends among low-SES individuals, averaged over all low-SES individuals in the ZIP code"
+df['clustering_zip_meaning'] = 'The average fraction of an individual\s friend pairs who are also friends with each other'
+df['volunteering_zip_meaning'] = 'The percentage of Facebook users who are members of a \'volunteering\' or \'activism\' group'
 
 
 # Step 6: Remove remaining unnecessary columns from the dataframe.
-
-df = df.drop(columns=['exposure_grp_mem_zip','exposure_grp_mem_high_zip', 'nbhd_exposure_zip',
+df = df.drop(columns=['ec_se_zip','nbhd_ec_zip','ec_grp_mem_zip','ec_high_zip','ec_high_se_zip','nbhd_ec_high_zip','ec_grp_mem_high_zip','exposure_grp_mem_zip','exposure_grp_mem_high_zip', 'nbhd_exposure_zip',
        'bias_grp_mem_zip', 'bias_grp_mem_high_zip', 'nbhd_bias_zip', 'nbhd_bias_high_zip',
        'support_ratio_zip', 'state_fips'])
 
+df = df.rename(columns={
+    'num_below_p50': 'Number Below Median',
+    'pop2018': 'Population in 2018',
+    'ec_zip': 'Economic Connectedness',
+    'clustering_zip': 'Clustering',
+    'volunteering_rate_zip': 'Volunteering Rate',
+})
 
-
-
+## **Build Interaction**
 alt.data_transformers.disable_max_rows() #Disabling MaxRowsError to load large dataset
 
-st.title("Social Capital Data Interaction")
-@st.cache  # add caching so we load the data only once
+st.title("What are the features of the geographic distribution of social capital metrics?")
+st.write("Data source: [Social Capital II: Determinants of Economic Connectedness](https://opportunityinsights.org/paper/social-capital-ii-determinants-of-economic-connectedness/)")
 
+show_raw_data = st.checkbox("Click to show raw data")
 
-
-st.write("Let's look at raw data in the Pandas Data Frame.")
-# TO DO: display all rows in charts
-# df = df.head(200) # get the first 1000 rows to data to display charts successfully (if too large the chat fails to display)
-st.write(df)
-st.write("Total number of zipcodes: "+str(len(df.index)))
+if show_raw_data:
+    st.write("Total number of zipcodes: "+str(len(df.index)))
+    st.write(df[["zip", "Location", "Number Below Median", "Population in 2018", "Economic Connectedness", "Clustering", "Volunteering Rate"]])
 
 # change the webpage's css style to display radio buttons horizontally
 # reference: https://discuss.streamlit.io/t/horizontal-radio-buttons/2114/3
 st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
 
+## Part 1: Map Chart to show each zipcode as one point
 # create radio buttons for users to select one dimension
 dimension = st.radio("Select one dimension of social capital",("Economic Connectedness", "Cohesiveness", "Civic Engagement"))
 
@@ -150,13 +144,18 @@ background = alt.Chart(states).mark_geoshape(
 
 map_title = dimension + " by Zipcode"
 dimension_dic = {
-    "Economic Connectedness": "ec_zip",
-    "Cohesiveness": "clustering_zip", 
-    "Civic Engagement":"volunteering_rate_zip"
-    }
-map_tooltip_value = dimension_dic[dimension]
+    "Economic Connectedness": "Economic Connectedness",
+    "Cohesiveness": "Clustering", 
+    "Civic Engagement":"Volunteering Rate"
+}
 
-st.write(str(dimension)+" is measured by "+str(dimension_dic[dimension])+", the percentage of ..")
+descp_dict = { "Economic Connectedness": "two times the share of high-SES friends among low-SES individuals, averaged over all low-SES individuals in the ZIP code",
+    "Cohesiveness": "the average fraction of an individual\s friend pairs who are also friends with each other", 
+    "Civic Engagement": " the percentage of Facebook users who are members of a 'volunteering' or 'activism' group"
+}
+description = dimension_dic[dimension]
+
+st.write(str(dimension)+" is measured by "+str(dimension_dic[dimension])+", "+str(descp_dict[dimension]))
 
 # the data points on the map background
 # TO DO: update column label names
@@ -166,10 +165,10 @@ points = alt.Chart(df).mark_circle().encode(
     longitude = 'lng',
     latitude = 'lat',
     size = alt.value(10),
-    tooltip = ['zip', map_tooltip_value],
-    color=alt.Color(map_tooltip_value, 
+    tooltip = ['zip', dimension_dic[dimension]],
+    color=alt.Color(dimension_dic[dimension], 
                     scale=alt.Scale(
-                        domain=[df[map_tooltip_value].min(), df[map_tooltip_value].max()], 
+                        domain=[df[dimension_dic[dimension]].min(), df[dimension_dic[dimension]].max()], 
                         range=['red', 'orange','yellow','green','blue'])),
 ).properties(
     width = 600,
@@ -177,9 +176,21 @@ points = alt.Chart(df).mark_circle().encode(
     title = map_title
 ).add_selection(single_brush)
 
+
+## Part 2: Table that displaying specific info for the selected zip code:
+#   -location
+#   -num_below_p50 (% of population under 50% income percentile)
+#   -ec_zip (economic connectedness score)
+#   -clustering_zip (proportion of a person's friends who are friends with each other)
+#   -civic_organizations_zip (proportion of people who are members of a civic organization)
+st.title("Social Capital Metrics of Selected Zip Code")
+
 # text to show detailed information of one zipcode
 # reference: https://altair-viz.github.io/gallery/scatter_linked_table.html
-zip_text = alt.Chart(df).mark_text().encode(
+zip_text = alt.Chart(df).mark_text(
+    #align='right',
+).encode(
+    x=alt.X(axis=None),
     y=alt.Y('row_number:O',axis=None)
 ).transform_window(
     row_number='row_number()'
@@ -190,60 +201,65 @@ zip_text = alt.Chart(df).mark_text().encode(
 ).transform_filter(
     alt.datum.rank<2
 )
-economic_connectedness = zip_text.encode(text='ec_zip').properties(title='Economic Connectedness')
-cohesiveness = zip_text.encode(text='clustering_zip').properties(title='Cohesiveness')
-civic_engagement = zip_text.encode(text='volunteering_rate_zip').properties(title='Civic Engagement')
+zipcode = zip_text.encode(text='zip', tooltip='zip_meaning').properties(title="Zipcode")
+location = zip_text.encode(text='Location', tooltip='location_meaning').properties(title='Location (State, County, City)')
+num_below = zip_text.encode(text='Number Below Median', tooltip='num_below_p50_meaning').properties(title='Number Below Median')
+population = zip_text.encode(text='Population in 2018', tooltip='population_2018_meaning').properties(title='Population in 2018')
 
-text_area = alt.hconcat(
+economic_connectedness = zip_text.encode(text='Economic Connectedness', tooltip='ec_zip_meaning').properties(title='Economic Connectedness')
+cohesiveness = zip_text.encode(text='Clustering',tooltip='clustering_zip_meaning').properties(title='Cohesiveness')
+civic_engagement = zip_text.encode(text='Volunteering Rate', tooltip='volunteering_zip_meaning').properties(title='Civic Engagement')
+
+
+basic_info = alt.hconcat(
+    zipcode,
+    location,
+    num_below,
+    population,
+)
+
+# basic_info = alt.HConcatChart(
+#     hconcat = [zipcode, location, num_below, population],
+#     autosize = 'fit'
+# )
+
+
+social_capital_metric = alt.hconcat(
     economic_connectedness,
     cohesiveness,
     civic_engagement
-    )
-map_with_text = alt.vconcat(
-    background+points,
-    text_area
 )
-st.write(map_with_text)
 
+text_area = alt.vconcat(
+    basic_info,
+    social_capital_metric,
+    center = True
+)
 
-## Make a table that displays (for the selected zip code):
-#   -zip code
-#   -num_below_p50 (% of population under 50% income percentile)
-#   -ec_zip (economic connectedness score)
-#   -clustering_zip (proportion of a person's friends who are friends with each other)
-#   -civic_organizations_zip (proportion of people who are members of a civic organization)
+metrics = ['Economic Connectedness Percentile', 'Clustering Percentile','Volunteering Rate Percentile']
 
-individual_df = df[['zip','num_below_p50','ec_zip','clustering_zip','civic_organizations_zip']]
-individual_df = individual_df.rename(columns={'num_below_p50':'numer of children under 50% income percentile','ec_zip':'economic connectedness score','clustering_zip':'proportion of a person\'s friends who are friends with each other','civic_organizations_zip':'proportion of people who are members of a civic organization'})
-individual_df = individual_df.set_index('zip')
-
-
-st.title("Social Capital Metrics of Selected Zip Code")
-#   **To Do**
-#   Change the iloc to a variable set equal to the index of the zip code selected in the map.
-
-st.table(individual_df.iloc[0:10])
-
-## Make a bar graph that displays the selected zip code's percentile
-#   for each of the 3 social capital indicators.
-#   On hover, show the raw score for that indicator.
-#   TO DO: find a way to connect the map chart with bar graph together. Might using brush instead of an individual data point
-
-source = pd.DataFrame({
-    'Social Capital Metric': ['Economic Connectedness', 'Cohesiveness', 'Civic Engagement'],
-    'Percentile': [individual_df['ec_zip_percentile'].iloc[0], individual_df['clustering_zip_percentile'].iloc[0], individual_df['civic_organizations_zip_percentile'].iloc[0]],
-    'Raw Metric Score': [individual_df['economic connectedness score'].iloc[0], individual_df['proportion of a person\'s friends who are friends with each other'].iloc[0], individual_df['proportion of people who are members of a civic organization'].iloc[0]]
-})
-
-source_chart = alt.Chart(source).mark_bar().encode(
-    x='Percentile:Q',
-    y='Social Capital Metric:N',
-    tooltip=['Raw Metric Score:Q']
+bar = alt.Chart(df).transform_fold(
+    fold = metrics
+).mark_bar().encode(
+    x = alt.X('key:N', 
+        title = "Social Capital Metrics", 
+        axis=alt.Axis(labelAngle=0),
+        sort = metrics),
+    y = alt.Y('value:Q',
+        title = "Percentile",
+        #scale=alt.Scale(domain=[0, 1]),
+        ), 
+    color = alt.Color('key:N',sort = metrics),
+    tooltip ='value:Q',
 ).properties(
     width=600,
     height=400,
     title='Social Capital Percentiles for Selected Zipcode'
-).interactive()
+).transform_filter(single_brush)
 
-st.altair_chart(source_chart, use_container_width=True)
-
+map_with_text = alt.vconcat(
+    background+points,
+    text_area,
+    bar
+)
+st.write(map_with_text)
